a6e8a7b32a895887c8b107d7d903d5a93d9462ba
Roem D. Fegi <56340201+roemfegi123@users.noreply.github.com>
2022-06-20 02:12:06 +0800

    Add files via upload

---
+++ precache-manifest.f692d58c191c7b143d5406046b6db930.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e05c9d0e916935d144c",
+    "url": "/static/css/main.4dc6e739.chunk.css"
+  },
+  {
+    "revision": "1e05c9d0e916935d144c",
+    "url": "/static/js/main.1e05c9d0.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "9069e2cd9656e2c1047a",
+    "url": "/static/css/2.84a1f5c2.chunk.css"
+  },
+  {
+    "revision": "9069e2cd9656e2c1047a",
+    "url": "/static/js/2.9069e2cd.chunk.js"
+  },
+  {
+    "revision": "9c15d78cc6e756e21791",
+    "url": "/static/js/runtime~main.9c15d78c.js"
+  },
+  {
+    "revision": "be5ac74685407500368c9eeaeb18e24a",
+    "url": "/static/media/logo1.be5ac746.png"
+  },
+  {
+    "revision": "ec6b77b841d79dfddc631018fb838328",
+    "url": "/static/media/em.ec6b77b8.png"
+  },
+  {
+    "revision": "21b365838bdd763cf5f5bcba0bba93fa",
+    "url": "/static/media/pfl.21b36583.jpg"
+  },
+  {
+    "revision": "7245059722339d7202e8b7fe17e00e15",
+    "url": "/static/media/E1.72450597.jpg"
+  },
+  {
+    "revision": "22311b6ff0e7bb7ecb6d35addba3de8c",
+    "url": "/static/media/F1.22311b6f.jpg"
+  },
+  {
+    "revision": "cb6cdc708d5d55299592a94358d4b5f7",
+    "url": "/static/media/M3.cb6cdc70.png"
+  },
+  {
+    "revision": "d497ff4e499a5aa079876eba1c1f45dc",
+    "url": "/static/media/Y1.d497ff4e.jpg"
+  },
+  {
+    "revision": "2bcb54c3a87f5ee9dcdab2ea3e155044",
+    "url": "/static/media/C1.2bcb54c3.jpg"
+  },
+  {
+    "revision": "d3b50dfa2bcf36c44f75a1f89c07b4da",
+    "url": "/static/media/CC1.d3b50dfa.png"
+  },
+  {
+    "revision": "04216e03988b48f3a282b9826ec9b626",
+    "url": "/static/media/wave.04216e03.svg"
+  },
+  {
+    "revision": "770ffb563f5de1e572d4b6faae7cac51",
+    "url": "/static/media/bg1.770ffb56.jpg"
+  },
+  {
+    "revision": "5c023a52bc4d1444dab45bb057c335a5",
+    "url": "/index.html"
+  }
+];
